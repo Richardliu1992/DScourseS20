f792e12abb7952d6e98591647e2ff38f0375556c
Ruichun Liu <Richardliu1992@outlook.com>
2020-02-04 00:05:18 -0600

    Complete PS3

---
+++ ProblemSets/PS3/PS3_LIU.sql
@@ -0,0 +1,10 @@
+.mode csv
+.import /home/ouecon030/DScourseS20/ProblemSets/PS3/FL_insurance_sample.csv datname
+
+SELECT * FROM datname LIMIT 10;
+
+SELECT DISTINCT county FROM datname;
+
+SELECT AVG(tiv_2012 - tiv_2011) FROM datname;
+
+SELECT construction, COUNT(*) FROM datname GROUP BY construction;
